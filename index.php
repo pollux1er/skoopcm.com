63679db322c104fc9a24536e9fa48e9fceb0b5f8
Patient Assontia <assontia@gmail.com>
2016-04-02 23:18:46 +0100

    Initial Version
    Version initiale

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">		
+  <title>SKOOP.CM | Rien ne vous echappe - Tres bientot</title>
+  <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>	
+  <!--<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">-->	
+</head>
+<body>
+	<div id="u348-4" class="clearfix grpelem"><!-- content -->
+	      <p>RIEN NE VOUS ECHAPPE ...!</p>
+	</div>
+	<div id="u349-4" class="clearfix colelem"><!-- content -->
+         <p>Bientot disponible en téléchargements...</p>
+    </div>
+<img src="image/Oeil2.png" class="ri" />	
+</body>
+</html>
