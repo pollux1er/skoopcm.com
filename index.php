f7eadc20ffaa29d52858c83e88de1e0e26481c56
Patient Assontia <assontia@gmail.com>
2016-04-03 02:41:27 +0100

    Version mobile

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="UTF-8">		
-  <title>SKOOP.CM | Rien ne vous echappe - Tres bientot</title>
-  <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>	
-  <!--<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">-->	
+	<meta charset="UTF-8">		
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />  
+	<title>SKOOP.CM | Rien ne vous echappe - Tres bientot</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen, handheld" />
+	<!--[if (lt IE 9)&(!IEMobile)]>
+		<link rel="stylesheet" type="text/css" href="mobile.css" />
+	<![endif]-->
+	<!--<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">-->	
 </head>
 <body>
+	<img src="image/Oeil2.png" class="ri" />
 	<div id="u348-4" class="clearfix grpelem"><!-- content -->
 	      <p>RIEN NE VOUS ECHAPPE ...!</p>
 	</div>
 	<div id="u349-4" class="clearfix colelem"><!-- content -->
          <p>Bientot disponible en téléchargements...</p>
     </div>
-<img src="image/Oeil2.png" class="ri" />	
+		
 </body>
 </html>
